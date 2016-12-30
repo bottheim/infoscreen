4806fc573842438764eac1c7d54d73a40f8c00a3
Johan Aamdal Bottheim <bottheim@gmail.com>
2016-12-30 20:28:58 +0100

    Add files via upload

---
+++ sleep_wake_06_script.sh
@@ -0,0 +1,10 @@
+TOMORROW=$(date -v+1d "+%m/%d/%y")
+TODAY=$(date "+%m/%d/%y")
+
+# echo "Starting job $TOMORROW 06:45:00"
+# echo "Starting job $TODAY 16:00:00"
+
+# pmset schedule wake "$TOMORROW 06:45:00" 
+ pmset schedule wake "$TODAY 16:00:00"
+
+ pmset sleepnow
